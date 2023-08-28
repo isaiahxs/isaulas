f29cbb0a6cb44e1b53c9e0fec657464dba35b776
isaiah <isaiahsinnathamby@gmail.com>
2023-08-27 22:30:54 -0400

    improved UI on services

--- src/components/GlenBurnie/GBServices/index.js
+++ src/components/GlenBurnie/GBServices/index.js
@@ -2,89 +2,6 @@ import './GBServices.css';
 
 export default function GBServices() {
     return (
-        // <div className="services-section">
-        //     <div className="home-header-container">
-        //         <h2 className="home-header">Our Services</h2>
-        //     </div>
-        //     <div>first come first serve, appointments too</div>
-        //     <div className="female-cuts">
-        //         $30 Haircuts
-        //         Keratin Straightening
-        //         Tint
-        //         Highlights
-        //         Perms
-        //         Blow Dry
-        //         Styling
-        //         Eyebrows
-        //         Makeup
-        //     </div>
-
-        //     <div className='male-cuts'>
-        //         Kids Haircut $20
-        //         Haircut $25
-        //         Haircut and Eyebrows $25
-        //         Haircut and Beard $30
-        //     </div>
-        // </div>
-
-
-
-
-
-
-        // <div className="services-section">
-        //     <div className="home-header-container services-container">
-        //         <h2 className="home-header services-heading">Our Services</h2>
-        //         <h3 className='services-subheading'>First come, first served. Appointments available.</h3>
-        //     </div>
-
-        //     <div className='services-tables'>
-        //         <div className="female-cuts">
-        //             <div className='table-heading-container'>
-        //                 <h3 className='table-heading'>Female Cuts</h3>
-        //             </div>
-        //             <table>
-        //                 <tr>
-        //                     <td>$30 Haircuts</td>
-        //                     <td>Keratin Straightening</td>
-        //                     <td>Tint</td>
-        //                     <td>Highlights</td>
-        //                 </tr>
-        //                 <tr>
-        //                     <td>Perms</td>
-        //                     <td>Blow Dry</td>
-        //                     <td>Styling</td>
-        //                     <td>Eyebrows</td>
-        //                 </tr>
-        //                 <tr>
-        //                     <td>Makeup</td>
-        //                     <td></td>
-        //                     <td></td>
-        //                     <td></td>
-        //                 </tr>
-        //             </table>
-        //         </div>
-
-        //         <div className='male-cuts'>
-        //             <div className='table-heading-container'>
-        //                 <h3 className='table-heading'>Male Cuts</h3>
-        //             </div>
-        //             <table>
-        //                 <tr>
-        //                     <td>Kids Haircut $20</td>
-        //                     <td>Haircut $25</td>
-        //                 </tr>
-        //                 <tr>
-        //                     <td>Haircut and Eyebrows $25</td>
-        //                     <td>Haircut and Beard $30</td>
-        //                 </tr>
-        //             </table>
-        //         </div>
-        //     </div>
-        // </div>
-
-
-
         <div className="services-section">
             <div className="home-header-container services-container">
                 <h2 className="home-header services-heading">Our Services</h2>
@@ -94,8 +11,9 @@ export default function GBServices() {
             <div className='services-lists'>
                 <div className="female-cuts">
                     <h3 className='list-heading'>Women's Styles</h3>
+                    <h4 className='list-subheading'>Prices vary</h4>
                     <ul className='style-list'>
-                        <li>$30 Haircuts</li>
+                        <li>Haircuts $30</li>
                         <li>Keratin Straightening</li>
                         <li>Tint</li>
                         <li>Highlights</li>
